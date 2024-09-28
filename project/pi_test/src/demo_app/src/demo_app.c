4f7a6524b05d477c91da86c1f0b0797800627649
Myskip <myskip@163.com>
2024-09-28 20:40:15 +0800

    feat : demo app使用rblog

--- project/pi_test/src/demo_app/src/demo_app.c
+++ project/pi_test/src/demo_app/src/demo_app.c
@@ -3,13 +3,17 @@
 #include <stdbool.h>
 #include "demo_app.h"
 #include "demo_component.h"
-// #include "rblog.h"
+#include "rblog.h"
 
 int main()
 {
     printf("DEBUG:%d, TAG:%d\n", DEBUG, TAG);
 
     demo_component_init();
+
+    rblog_init();
+
+    rblog_d("DEMO", "Hello, world!");
 
     return 0;
 }
